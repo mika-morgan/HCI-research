71834711e496f4ceb91409d8c509024da3e6526b
Mika Morgan <112218152+mika-morgan@users.noreply.github.com>
2024-02-05 13:04:32 -0600

    Update hci_scrape_apple.py

--- hci_scrape_apple.py
+++ hci_scrape_apple.py
@@ -4,11 +4,11 @@ import json
 #Create the output file to write the comments, scores, and subreddits to
 f1 = open("apple_codespark.txt", 'w', encoding='utf-8')
 
-minecraft = AppStore(country="us", app_name="codespark")
-minecraft.review()
+cs = AppStore(country="us", app_name="codespark")
+cs.review()
 
-for review in minecraft.reviews:
+for review in cs.reviews:
     f1.write(json.dumps(review, default=str))
     f1.write('\n')
     f1.write('\n')
-print(minecraft.reviews_count)
+print(cs.reviews_count)
